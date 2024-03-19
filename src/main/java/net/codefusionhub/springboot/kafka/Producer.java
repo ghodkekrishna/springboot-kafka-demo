1455f85de45f09c0d875f2e97b32cec656f1da38
Krishna Ghodke <krushnaghodke90@gmail.com>
2024-03-19 15:15:23 -0700

    create Kafka producer and configs

---
+++ src/main/java/net/codefusionhub/springboot/kafka/Producer.java
@@ -0,0 +1,22 @@
+package net.codefusionhub.springboot.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Producer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Producer.class);
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent %s", message));
+        kafkaTemplate.send("codefusionhub", message);
+    }
+}
