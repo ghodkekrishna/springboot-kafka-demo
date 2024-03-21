5dfde45683de1257ee2d93fcd475fedba5bfa09b
Krishna Ghodke <krushnaghodke90@gmail.com>
2024-03-20 20:51:08 -0700

    Create Kafka Consumer

---
+++ src/main/java/net/codefusionhub/springboot/kafka/Consumer.java
@@ -0,0 +1,17 @@
+package net.codefusionhub.springboot.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
+
+    @KafkaListener(topics = "codefusionhub", groupId = "myGroup")
+    public void consume(String message){
+        LOGGER.info(String.format("Message received %s", message));
+    }
+}
